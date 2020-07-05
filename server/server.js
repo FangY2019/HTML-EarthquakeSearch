e10998a397c05c8a2ccd4747b9534528e9e7d185
Fang Ye <yef10@mcmaster.ca>
2020-07-05 15:14:02 -0400

    document server file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+// create express server
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+//link to mysql database
 var mysql = require('mysql');
 var mysqlconnection = mysql.createConnection({
     host     : 'localhost',
@@ -13,34 +15,34 @@ var mysqlconnection = mysql.createConnection({
 var  sql = 'SELECT * FROM earthquake';
 mysqlconnection.connect();
 
-
-app.use( bodyParser.json() ); 
-app.get('/search.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "search.html" );
-})
-
+//request data and respond to the client request
 app.post('/earthquakelist',urlencodedParser, function (req, res) {
+    //gets the input in the form 
     global.lat = req.body.latitude;
     global.lng = req.body.longitude;
     global.radius = req.body.radius;
+    //query in mysql
     mysqlconnection.query(sql,function (err, results) {
         var count = 0;
         if (!err) {
-            res.write("Date" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "Latitude" + "&nbsp;&nbsp;&nbsp;" + "Longitude" + "&nbsp;&nbsp;&nbsp;" + "Magnitude" +"<br>");
+            res.write("Date" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                      "Latitude" + "&nbsp;&nbsp;&nbsp;" + 
+                      "Longitude" + "&nbsp;&nbsp;&nbsp;" + 
+                      "Magnitude" +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + 
+                      "Place" +"<br>");
             for(var i = 0; i < results.length; i++){
                 if(distanceTo(parseFloat(results[i].latitude), parseFloat(results[i].longitude)) < radius){
-                    var line = results[i].date.substring(0, 10) +"&nbsp;&nbsp;&nbsp;" + 
-                    parseFloat(results[i].latitude).toFixed(3)+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
-                    parseFloat(results[i].longitude).toFixed(3)+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
-                    parseFloat(results[i].magnitude).toFixed(1)+
+                    var line = results[i].date.substring(0, 10) + "&nbsp;&nbsp;&nbsp;" + 
+                    parseFloat(results[i].latitude).toFixed(3) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+                    parseFloat(results[i].longitude).toFixed(3) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+                    parseFloat(results[i].magnitude).toFixed(1) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+                    results[i].place +
                     "<br>";
                     res.write(line);
                     count++;
-                }
-                
-            }
-            
-            res.end("The number of historical earthquakes within " +  radius + " km is: " + count);
+                }                
+            }            
+            res.end("<br>The number of historical earthquakes within " +  radius + " km is: " + count);
         }
         else{
             console.log(err);
@@ -48,6 +50,7 @@ app.post('/earthquakelist',urlencodedParser, function (req, res) {
     });   
 })
 
+//gets the distance from the input location to a location of a specific historical earthquake location 
 function distanceTo(lat2, lng2){
     var earthRadius = 6371;
 	var deltaLat = degToRad(lat2 - lat);
@@ -59,10 +62,12 @@ function distanceTo(lat2, lng2){
 	return distance;
 }
 
+//conver a degree to radian
 function degToRad(degrees) {
     return degrees * (Math.PI / 180);
   };
 
+//bond the port
 app.listen(3000, function() {
     console.log('listening on port 3000!');
   });
